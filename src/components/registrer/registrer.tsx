94a26a484ba15eb90d6071b4dff1352a89d659fd
Matias <matiasgaitan370@gmail.com>
2026-06-11 15:33:02 -0300

    Creación de login.tsx y eliminación de login.ts

--- src/components/registrer/registrer.tsx
+++ src/components/registrer/registrer.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./Register.css";
+import "./register.css";
 import type { Usuario } from "../../interfaces/Usuario";
 
 const Register: React.FC = () => {
